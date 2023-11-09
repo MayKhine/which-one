9e88d10c127c107b32b4719dc73a57d2f82f0501
May <maykhine.ada@gmail.com>
2023-11-09 18:05:17 -0500

    debugging posts and user useeffect

--- src/components/Post/PostByUser.tsx
+++ src/components/Post/PostByUser.tsx
@@ -4,32 +4,33 @@ import { useUserApi } from "../../hooks/useUserApi"
 
 type PostByUserProps = {
   post: PostProps
+  onDeleteFn: () => void
 }
 
-export const PostByUser = ({ post }: PostByUserProps) => {
-  const { user } = useUserApi("user1")
-  const userName = user?.name
+export const PostByUser = ({ post, onDeleteFn }: PostByUserProps) => {
+  // const { user } = useUserApi("user1")
+  // const userName = user?.name
 
-  const deletePost = async () => {
-    const result = await fetch(
-      `http://localhost:3300/users/${userName}/posts/${post.id}?delete=true`,
-      {
-        method: "Delete",
-      }
-    )
-    const response = await result
-    console.log("Delete Response: ", response)
-  }
+  // const deletePost = async () => {
+  //   const result = await fetch(
+  //     `http://localhost:3300/users/${userName}/posts/${post.id}?delete=true`,
+  //     {
+  //       method: "Delete",
+  //     }
+  //   )
+  //   const response = await result
+  //   console.log("Delete Response: ", response)
+  // }
 
-  const deletePostHandler = () => {
-    console.log("Delete Post Handler is called")
-    if (userName == post.userName) {
-      console.log("Delete Post Handler")
-      deletePost()
-    } else {
-      console.log("Cancel Delete Post Handler")
-    }
-  }
+  // const deletePostHandler = () => {
+  //   console.log("Delete Post Handler is called")
+  //   if (userName == post.userName) {
+  //     console.log("Delete Post Handler")
+  //     deletePost()
+  //   } else {
+  //     console.log("Cancel Delete Post Handler")
+  //   }
+  // }
   return (
     <div>
       <div>
@@ -38,7 +39,7 @@ export const PostByUser = ({ post }: PostByUserProps) => {
           return <li key={index}> {ans}</li>
         })}
       </div>
-      <Button type="submit" text="x" onClickFn={deletePostHandler} />
+      <Button type="submit" text="x" onClickFn={onDeleteFn} />
     </div>
   )
 }
--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -4,6 +4,7 @@ import { MenuBar } from "../UI/MenuBar"
 import { useParams } from "react-router-dom"
 import { NoUser } from "../components/Users/NoUser"
 import { PostByUser } from "../components/Post/PostByUser"
+import { useUserApi } from "../hooks/useUserApi"
 
 export const UserPage = () => {
   const [userExists, setUserExists] = useState<boolean>()
@@ -12,38 +13,78 @@ export const UserPage = () => {
   const { userName } = useParams<{ userName: string }>()
   // const [setPostsExist, setPostsExist] = useState<boolean>()
 
-  useEffect(() => {
-    const checkUserExist = async () => {
-      const result = await fetch(`http://localhost:3300/users/${userName}`, {
+  //
+  const checkUserExist = async () => {
+    const result = await fetch(`http://localhost:3300/users/${userName}`, {
+      method: "Get",
+    })
+
+    const response = await result.json()
+    // setUserExists(response.success)
+    return response.success
+  }
+
+  const getPostsByUser = async () => {
+    const result = await fetch(
+      `http://localhost:3300/users/${userName}/posts`,
+      {
         method: "Get",
-      })
-
-      const response = await result.json()
-      setUserExists(response.success)
-    }
-
-    const getPostsByUser = async () => {
-      const result = await fetch(
-        `http://localhost:3300/users/${userName}/posts`,
-        {
-          method: "Get",
-        }
-      )
-      const response = await result.json()
-      console.log(response)
-      if (response.success) {
-        setPostsExists(true)
-        setPosts(response.result)
-      } else {
-        setPostsExists(false)
       }
+    )
+    const response = await result.json()
+    console.log(response)
+    if (response.success) {
+      setPostsExists(true)
+      setPosts(response.result)
+    } else {
+      setPostsExists(false)
+    }
+  }
+
+  const { user } = useUserApi("user1")
+  const curUser = user?.name
+  const deletePost = async () => {
+    const result = await fetch(
+      `http://localhost:3300/users/${userName}/posts/${post.id}?delete=true`,
+      {
+        method: "Delete",
+      }
+    )
+    const response = await result
+    console.log("Delete Response: ", response)
+  }
+
+  const deletePostHandler = () => {
+    console.log("Delete Post Handler is called")
+    if (curUser == post.userName) {
+      console.log("Delete Post Handler")
+      deletePost()
+    } else {
+      console.log("Cancel Delete Post Handler")
+    }
+  }
+
+  useEffect(() => {
+    // checkUserExist()
+    // if (userExists) {
+    // }
+
+    const checkUser = async () => {
+      const result = await checkUserExist()
+      setUserExists(result)
     }
 
-    checkUserExist()
-    if (userExists) {
-      getPostsByUser()
+    const getPosts = async () => {
+      await getPostsByUser()
     }
-  }, [userName, userExists, posts])
+
+    checkUser()
+    console.log("UserExists", userExists)
+
+    if (userExists) {
+      getPosts()
+    }
+  }, [])
 
   console.log("What is userName: ", userName, userExists, postsExists)
 
@@ -55,7 +96,11 @@ export const UserPage = () => {
         postsExists &&
         posts.map((post: PostProps, index: number) => {
           return (
-            <PostByUser post={post} key={index} />
+            <PostByUser
+              post={post}
+              key={index}
+              onDeleteFn={deletePostHandler}
+            />
             // <div style={{ backgroundColor: "orange" }} key={index}>
             //   {post.userName}
             //   {post.question}
