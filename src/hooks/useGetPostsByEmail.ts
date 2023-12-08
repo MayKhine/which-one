ca39d586b161ef99b140352db800739674965d81
May <maykhine.ada@gmail.com>
2023-12-08 12:24:47 -0500

    get posts by email: working

--- src/hooks/useGetPostsByEmail.ts
+++ src/hooks/useGetPostsByEmail.ts
@@ -8,8 +8,11 @@ export const useGetPostsByEmail = (email: useGetPostsByEmailProps) => {
   const [posts, setPosts] = useState<Array<PostProps>>([])
   console.log("EMAIL IN HOOK: ", email)
   const getPostsByEmail = useCallback(async () => {
-    const result = await fetch(`http://localhost:3300/posts?email=${email}`)
+    const result = await fetch(
+      `http://localhost:3300/postsbyemail?email=${email}`
+    )
     const response = await result.json()
+    console.log("WHAT IS RESPOSNT: ", response)
     if (response.success) {
       setPosts(response.result)
     }
@@ -19,6 +22,6 @@ export const useGetPostsByEmail = (email: useGetPostsByEmailProps) => {
     getPostsByEmail()
   }, [getPostsByEmail])
 
-  console.log("HOOK: posts: ", posts, email)
+  console.log("Email in Hook: posts: ", posts, email)
   return [posts, getPostsByEmail] as const
 }
